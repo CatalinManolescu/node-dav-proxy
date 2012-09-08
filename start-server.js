795f2340f6c2226880a0335860f24593d335a944
Catalin Manolescu <cc.manolescu@gmail.com>
2012-09-08 22:45:49 +0300

    add session support and forwarding to webdav

---
+++ start-server.js
@@ -0,0 +1,7 @@
+/**
+ * @author Catalin Manolescu <cc.manolescu@gmail.com>
+ * @since 05 September 2012 - 10:12
+ */
+
+var proxy = require('./lib/Proxy.js');
+proxy.init(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6]);
