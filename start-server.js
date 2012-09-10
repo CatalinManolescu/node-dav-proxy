9a493ec622d2330d339a660590f8c4329f1969d3
Catalin Manolescu <cc.manolescu@gmail.com>
2012-09-10 13:14:13 +0300

    fix upload

--- start-server.js
+++ start-server.js
@@ -4,4 +4,8 @@
  */
 
 var proxy = require('./lib/Proxy.js');
-proxy.init(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6]);
+proxy.init(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6], postRequestHandler);
+
+function postRequestHandler( command ) {
+    console.log(command);
+}
