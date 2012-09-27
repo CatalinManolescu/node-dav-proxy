68bad754364c1ca3a5f8d973f8074941f4a76e06
Catalin Manolescu <cc.manolescu@gmail.com>
2012-09-27 15:20:04 +0300

    changed router; refactoring

--- start-server.js
+++ start-server.js
@@ -3,8 +3,10 @@
  * @since 05 September 2012 - 10:12
  */
 
-var proxy = require('./lib/Proxy.js');
+var proxy = require('./lib/server.js');
 proxy.init(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6], postRequestHandler);
+
+proxy.enableDevelopmentMode(true);
 proxy.createSession('demo', 'demo', 'WebDAV');
 
 function postRequestHandler( command ) {
