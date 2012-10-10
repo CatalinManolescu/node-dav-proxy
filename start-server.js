1a28ed361975cd2875983a48629cdf3471c416ed
Catalin Manolescu <cc.manolescu@gmail.com>
2012-10-10 13:38:08 +0300

    changed auth method

--- start-server.js
+++ start-server.js
@@ -3,12 +3,12 @@
  * @since 05 September 2012 - 10:12
  */
 
-var proxy = require('./lib/server.js');
-proxy.init(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6], postRequestHandler);
+var proxy       = require('./lib/server.js');
+proxy.init(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6], afterRequestHandler);
 
 proxy.enableDevelopmentMode(true);
 proxy.createSession('demo', 'demo', 'WebDAV');
 
-function postRequestHandler( command ) {
+function afterRequestHandler( command ) {
     console.log(command);
 }
